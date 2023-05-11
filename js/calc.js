584c18b1d6164c43ef8b097049cdece8cbca7b6d
Mmateus106 <rm98524@fiap.com.br>
2023-05-11 17:04:53 -0300

    CSS e javascript da calculadora

---
+++ js/calc.js
@@ -0,0 +1,13 @@
+
+
+//CRIANDO UMA FUNÇÃO PARA RECEBER O BOTÃO
+function insereNumero(botao,visor) {
+    visor.value += botao.value;
+}
+
+function resultado(visor) {
+
+    console.log(visor.value);
+    let novoVisor = visor.value.split()
+
+}
