c8f80af2202e8747cc6451ffacef2043e1c59b60
Mmateus106 <rm98524@fiap.com.br>
2023-05-17 11:06:28 -0300

    calculadora atualizada

--- js/calc.js
+++ js/calc.js
@@ -1,13 +1,23 @@
 
-
-//CRIANDO UMA FUNÇÃO PARA RECEBER O BOTÃO
 function insereNumero(botao,visor) {
-    visor.value += botao.value;
+    
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+        
+       
+
+        if(visor.value[visor.value.length-1] == "+" ||  visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+            visor.value.slice(visor.value.length-1);
+        }else{
+            visor.value += botao.value;
+        }
+
+    }else{
+        visor.value += botao.value;
+    }
+
 }
 
 function resultado(visor) {
-
-    console.log(visor.value);
-    let novoVisor = visor.value.split()
-
+    visor.value = eval(visor.value);
 }
+
